bac53c15cc566c65818dabe4725669f9c74e54dd
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 01:49:18 +0100

    fix form call

--- content.js
+++ content.js
@@ -2,8 +2,6 @@ function msgprint() {
             alert("You are Successfully Called the JavaScript function");  
          }  
 
-function setAction(form) {
-  form.action = "./AIR_ASSIGNMENT3/index.html";
-  alert(form.action);
-  return false;
-}
+function main(input) {  
+            alert("You are Successfully Called the JavaScript function");  
+         }  
