d3a14ce73a6946fa31b1e3bca2e0c85676f56b0c
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 02:10:39 +0100

    fix path bug

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
 function msgprint() {  
             alert("You are Successfully Called the JavaScript function");  
+         }
+
+function main() {
+			alert("You are Successfully Called the JavaScript function2");
          }  
