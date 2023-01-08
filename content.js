fe87ac852c60de74e6a7cd2ce9805f253608b58a
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 01:28:40 +0100

    make form calling js

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
 function msgprint() {  
             alert("You are Successfully Called the JavaScript function");  
          }  
+
+function setAction(form) {
+  form.action = "register.html";
+  alert(form.action);
+  return false;
+}
