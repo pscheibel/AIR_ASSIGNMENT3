f20752cca760040053983de5025d5e5674d9943d
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 02:19:47 +0100

    fix implement main

--- content.js
+++ content.js
@@ -3,6 +3,6 @@ function msgprint() {
          }
 
 function main() {
-            let input=getElementById('in').innerHTML
+            let input=document.getElementById('in').innerHTML
 			alert(input);
          }
