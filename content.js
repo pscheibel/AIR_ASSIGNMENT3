219dc243b986a69224d26bd5d67f018cc469b0f6
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 03:06:52 +0100

    test pyscript outsourced v2

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ function msgprint() {
     alert("You are Successfully Called the JavaScript function");
 }
 
-function test_main() {
+function main() {
     let input=document.getElementById('in').innerHTML;
 	alert(input);
 }
