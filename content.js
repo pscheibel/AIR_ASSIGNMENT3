a95c412ab7c5133df6230b65cdb4cfcc64dad747
pscheibel <paul.scheibelmasser@aon.at>
2023-01-08 02:17:36 +0100

    implement main

--- content.js
+++ content.js
@@ -3,5 +3,6 @@ function msgprint() {
          }
 
 function main() {
-			alert("You are Successfully Called the JavaScript function2");
+            let input=getElementById('in').innerHTML
+			alert(input);
          }
