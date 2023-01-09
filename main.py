b7d0b1f59fa0d42853c4868845a318cf4043c20e
pscheibel <paul.scheibelmasser@aon.at>
2023-01-09 21:32:30 +0100

    reset and refactor settings

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from TestDataPreparation import TestDataPreparation
 import os
 
 CACHING_FILES_ENABLED = True
+DATASET_CREATION_ENABLED = True
+TRAINING_ENABLED = True
+INPUT_PREPARATION_ENABLED = True
 
 SCIENTIFICLABELS = {0: "Biology",
                     1: "Computer Science",
@@ -21,17 +24,18 @@ def main(loadPdfs, trainNN, prepareInput, inputPath="", modelPath="model.pt"):
     application = ApplyClassification(SCIENTIFICLABELS)
     if loadPdfs:
         preparation.executeLoading(CACHING_FILES_ENABLED, SCIENTIFICLABELS)
-    if trainNN or (not (os.path.exists("lookupdict.txt") and os.path.exists("model.pt") and os.path.exists("tfIdf.txt") and os.path.exists("idf.txt"))):
+    if trainNN or (not (os.path.exists("lookupdict.txt") and os.path.exists("model.pt") and os.path.exists(
+            "tfIdf.txt") and os.path.exists("idf.txt"))):
         trainData, testData, lookupDict = preparation.executePreparation(SCIENTIFICLABELS)
         training.startTraining(trainData, testData, len(lookupDict), SCIENTIFICLABELS, modelPath)
     data, size = reader.readInput(prepareInput, inputPath)
     application.classifyData(data, size, modelPath, inputPath)
-    #Output.plotRanking()
+    # Output.plotRanking()
     Output.subplotCorrectPredAndTfIdf()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main(False, True, True, "./input/")
+    main(DATASET_CREATION_ENABLED, TRAINING_ENABLED, INPUT_PREPARATION_ENABLED, "./input/")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
